0e602696e4fe823420dda9925827763545fbf643
Daniel Sundberg <sumpan@gmail.com>
2021-10-09 18:33:42 +0200

    On Linux, display error message if xclip is not found

--- src/extension.ts
+++ src/extension.ts
@@ -4,23 +4,29 @@
 import * as vscode from 'vscode';
 import * as copyPaste  from 'copy-paste';
 import * as path from 'path';
+import { sync as commandExists }  from 'command-exists';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
+
     console.log('Congratulations, your extension "copy-directory-path" is now active!');
 
     let disposable = vscode.commands.registerCommand('extension.copyDirectoryPath', () => {
+
+        if ((process.platform === "linux") && !commandExists('xclip')) {
+            vscode.window.showErrorMessage(`Xclip is required for this command to work!\nPlease run 'apt-get install xclip' or 'yum install xclip'...`);
+            console.error(`Xclip is required for this command to work!\nPlease run 'apt-get install xclip' or 'yum install xclip'...`);
+            return;
+        }
 
         const editor = vscode.window.activeTextEditor;
         if (!editor) {
             return;
         }
 
-        
         let filename = editor.document.fileName;
 
         // Extract and return directory path
